f6cbca7dd3164b49c1e1a75490023fad6e950b3b
Mabaragi <gigig9647@gmail.com>
2023-04-11 17:59:08 +0900

    0411

---
+++ 0411/class/building/hospitals/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Doctor(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} 전문의"
+    
+class Patient(models.Model):
+    doctors = models.ManyToManyField(Doctor, through='Reservation')
+    name =models.TextField()
+    
+    
+    def __str__(self):
+        return f"{self.name} 환자"
+    
+class Reservation(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    symtom = models.TextField()
+    reserved_at = models.DateTimeField(auto_now_add=True)
+
+# class Reservation(models.Model):
+#     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+#     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    
+#     def __str__(self):
+#         return f"{self.doctor_id}번 의사의 {self.patient_id}번 환자"
